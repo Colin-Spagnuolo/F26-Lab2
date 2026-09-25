0352a721df553e2f757efb623aad5351fc4c1673
Colin-Spagnuolo <cspagnuolo1@myseneca.ca>
2026-09-25 17:23:03 +0000

    completed Lab2h in Class

--- lab2h.py
+++ lab2h.py
@@ -1,14 +1,19 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Colin Spagnuolo
+# Date: September 25th
 # Purpose: Learn how to use while loops.
 # Usage: ./lab2h.py
 
 # TO DO 1: 
 # Creat variable timer.
+timer=10
 # The value of timer should initially be 10.
 # Use a while loop to create program that counts down from 10 with timer to 1.
+while timer>1:
+    print(timer)
+    timer=timer-1
+print("Blast Off!")
 # When you reach 1 end the loop and print blast off!
 
